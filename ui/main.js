856b75dbab10de88affa649636a689be97092551
shakeelakhtar <30746936+shakeelakhtar@users.noreply.github.com>
2017-08-14 21:25:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,12 @@
 
 //counter
  var button =document.getElementById('counter');
- var counter=0;
+ var counter= 0;
  button.onclick= function() {
      //make a request to the cunter end point
      //capture it in a variable
      //render the variable in a correct spam
-     counter=counter+1;
+     counter = counter + 1;
      var span=document.getElementById('count');
      span.innerHTML=counter.toString();
      
